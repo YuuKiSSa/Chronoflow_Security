c31aac6be733175b52e7a7e4b60f644a9671de6f
YuuKiSSa <e1241986@u.nus.edu>
2026-02-21 23:19:27 +0800

    spotless

--- notification-service/src/test/java/nus/edu/u/services/push/DeviceRegistryServiceImplTest.java
+++ notification-service/src/test/java/nus/edu/u/services/push/DeviceRegistryServiceImplTest.java
@@ -3,9 +3,7 @@ package nus.edu.u.services.push;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -23,7 +21,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.dao.DataIntegrityViolationException;
 
 @ExtendWith(MockitoExtension.class)
 class DeviceRegistryServiceImplTest {
