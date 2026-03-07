a8ce89d367233cf42fee64bb4f86b38ac5c1e45c
Shirley <chowjss@gmail.com>
2026-03-07 08:56:42 +0800

    Add file validation for magic number and content type

--- file-service/src/main/java/nus/edu/u/file/domain/vo/FileUploadReqVO.java
+++ file-service/src/main/java/nus/edu/u/file/domain/vo/FileUploadReqVO.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.List;
 import lombok.Data;
+import nus.edu.u.common.constant.SecurityConstants;
+import nus.edu.u.file.validators.ValidFile;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
@@ -18,6 +20,19 @@ public class FileUploadReqVO implements Serializable {
     @NotNull(message = "eventId is required")
     private Long eventId;
 
-    @NotEmpty(message = "files must not be empty")
+    @ValidFile(
+            maxSizeMB = SecurityConstants.MAX_FILE_UPLOAD_SIZE_IN_MB,
+            allowedTypes = {
+                    "image/jpeg",
+                    "image/png",
+                    "application/pdf",
+                    "text/plain",                                                                      // .txt
+                    "application/msword",                                                              // .doc
+                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",        // .docx
+                    "application/vnd.ms-excel",                                                        // .xls
+                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"               // .xlsx
+            },
+            message = "File validation failed"
+    )
     private List<MultipartFile> files;
 }
